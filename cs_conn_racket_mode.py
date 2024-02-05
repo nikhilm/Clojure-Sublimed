4e01b10c48a2e4a8be8dbc07b166fdd02814c6fa
Nikhil Marathe <me@nikhilism.com>
2024-02-05 13:00:59 -0800

    Some session opening, but ditching this in favor of LSP right now

--- cs_conn_racket_mode.py
+++ cs_conn_racket_mode.py
@@ -1,4 +1,4 @@
-import os, re, contextvars, subprocess, sublime, sublime_plugin
+import os, re, contextvars, subprocess, sublime, sublime_plugin, uuid, threading
 from . import cs_common, cs_conn, cs_eval, cs_parser, cs_printer
 
 server_process = contextvars.ContextVar("racket_mode_server_process", default=None)
@@ -19,9 +19,45 @@ class RacketModeServer:
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
         )
-        # TODO: Spin up a thread to monitor responses
+        self.shutdown = False
+        self.reader = threading.Thread(target=self.read_loop, daemon=True)
+        self.reader.start()
+        self.nonce = 0
+        print("The plugin is running in pid", os.getpid())
+
+    def next_nonce(self):
+        self.nonce += 1
+        return self.nonce
+
+    def read_loop(self):
+        # TODO: What to do about being blocked on a read?
+        # Use timeouts!
+        # How should reading work?
+        # Let's try out some to see what happens.
+        print("Running reader")
+        while True:
+            if self.shutdown:
+                break
+            # self.proc.communicate() doesn't work since it waits for the proc to exit
+            # Grr this can block
+            # in which case we can't shut down this thread
+            # perhaps we don't need to because of daemon?
+            print("Got stdout", self.proc.stdout.readline())
+            # Need an s-exp parser here.
+            # TODO: Also read stderr?
+            # How long to sleep for?
+
+    def send(self, session_id, msg):
+        self.proc.stdin.write(
+            # TODO: Open with encoding so we don't need this here.
+            f"({self.next_nonce()} {session_id} {msg})".encode("utf-8")
+        )
+        self.proc.stdin.flush()
 
     def terminate(self):
+        self.shutdown = True
+        # self.reader.join()
+        # TODO: Stop reader thread
         self.proc.kill()
 
 
@@ -30,7 +66,7 @@ class ConnectionRacketMode(cs_conn.Connection):
         super().__init__()
         self.status = None
         self.disconnecting = False
-        self.session_id = None  # TODO: Generate one
+        self.session_id = str(uuid.uuid4())
         self.window = sublime.active_window()
 
     def connect_impl(self):
@@ -39,6 +75,9 @@ class ConnectionRacketMode(cs_conn.Connection):
         if not server_process.get():
             print("Spawning new racket-mode server")
             server_process.set(RacketModeServer())
+        print("initiating a new repl with sesion id", self.session_id)
+        self.set_status(1, "Waiting for response")
+        server_process.get().send("#f", f"repl-start {self.session_id}")
         self.set_status(4, "Started server")
 
     def eval_impl(self, form):
@@ -46,7 +85,6 @@ class ConnectionRacketMode(cs_conn.Connection):
         cs_eval.on_success(form.id, "FAKE SUCCESS VALUE")
 
     def load_file_impl(self, id, file, path):
-        print("Load file request", id, file, path)
         cs_eval.on_success(id, "FAKE SUCCESS VALUE")
 
     def lookup_impl(self, id, symbol, ns):
